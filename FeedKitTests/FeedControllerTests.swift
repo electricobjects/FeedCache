39a7e3ff49766edafd72e941cd57b5882b4a7776
Rob Seward <rob@electricobjects.com>
2015-07-21 13:07:23 -0400

    Move mock classes into their own file

--- FeedKitTests/FeedControllerTests.swift
+++ FeedKitTests/FeedControllerTests.swift
@@ -11,55 +11,6 @@ import XCTest
 import FeedKit
 
 
-enum TestFeedKitType: FeedKitType {
-    case TestFeedType
-    
-    var cacheName : String {
-        return "test"
-    }
-    
-    func fetchItems(page: Int, itemsPerPage: Int, parameters: [String: AnyObject], success:(newItems:[FeedItem])->(), failure:(error: NSError)->()){
-        var items: [FeedItem] = []
-        if page == 1 {
-            items = [TestItem(name: "Foo"), TestItem(name: "Bar"), TestItem(name: "Baz")]
-        }
-        else if page == 2 {
-            items = [TestItem(name: "Baz"), TestItem(name: "Bing"), TestItem(name: "Boo")]
-        }
-    
-        success(newItems: items)
-    }
-}
-
-@objc
-class TestItem: FeedItem, NSCoding {
-    var name: String?
-    
-    init(name: String){
-        super.init()
-        self.name = name
-    }
-    
-    @objc required init(coder aDecoder: NSCoder){
-        name = aDecoder.decodeObjectForKey("name") as? String
-    }
-    
-    @objc func encodeWithCoder(aCoder: NSCoder){
-        aCoder.encodeObject(name, forKey: "name")
-    }
-    
-    var sortableReference: SortableReference {
-        return SortableReference(reference: self, hashValue: self.hashValue)
-    }
-    
-    override func isEqual(object: AnyObject?) -> Bool {
-        if let object = object as? TestItem {
-            return name == object.name
-        }
-        return false
-    }
-}
-
 
 
 class FeedControllerTests: XCTestCase, FeedKitDelegate {
@@ -72,6 +23,7 @@ class FeedControllerTests: XCTestCase, FeedKitDelegate {
         super.setUp()
         feedController = FeedController(feedType: TestFeedKitType.TestFeedType, cachingOn: true, section: 0)
         feedController?.cache?.addItems(testItems)
+        feedController?.cache?.waitUntilSynchronized()
     }
     
     override func tearDown() {
@@ -92,6 +44,7 @@ class FeedControllerTests: XCTestCase, FeedKitDelegate {
     }
 
 
+    
     
     //MARK: FeedKit Delegate methods
     
---
+++ FeedKitTests/MockClasses.swift
@@ -0,0 +1,60 @@
+//
+//  MockClasses.swift
+//  FeedKit
+//
+//  Created by Rob Seward on 7/21/15.
+//  Copyright © 2015 Rob Seward. All rights reserved.
+//
+
+import UIKit
+import FeedKit
+
+enum TestFeedKitType: FeedKitType {
+    case TestFeedType
+    
+    var cacheName : String {
+        return "test"
+    }
+    
+    func fetchItems(page: Int, itemsPerPage: Int, parameters: [String: AnyObject], success:(newItems:[FeedItem])->(), failure:(error: NSError)->()){
+        var items: [FeedItem] = []
+        if page == 1 {
+            items = [TestItem(name: "Foo"), TestItem(name: "Bar"), TestItem(name: "Baz")]
+        }
+        else if page == 2 {
+            items = [TestItem(name: "Baz"), TestItem(name: "Bing"), TestItem(name: "Boo")]
+        }
+        
+        success(newItems: items)
+    }
+}
+
+@objc
+class TestItem: FeedItem, NSCoding {
+    var name: String?
+    
+    init(name: String){
+        super.init()
+        self.name = name
+    }
+    
+    @objc required init(coder aDecoder: NSCoder){
+        name = aDecoder.decodeObjectForKey("name") as? String
+    }
+    
+    @objc func encodeWithCoder(aCoder: NSCoder){
+        aCoder.encodeObject(name, forKey: "name")
+    }
+    
+    var sortableReference: SortableReference {
+        return SortableReference(reference: self, hashValue: self.hashValue)
+    }
+    
+    override func isEqual(object: AnyObject?) -> Bool {
+        if let object = object as? TestItem {
+            return name == object.name
+        }
+        return false
+    }
+}
+
