e5283e9279f3bd658c5cfa2eee82e7f742675549
Rob Seward <rob@electricobjects.com>
2015-08-12 18:03:03 -0400

    Add mock API service to example app

---
+++ ExampleProject/ExampleProject/FeedItem.swift
@@ -0,0 +1,19 @@
+//
+//  FeedItem.swift
+//  ExampleProject
+//
+//  Created by Rob Seward on 8/12/15.
+//  Copyright © 2015 Electric Objects. All rights reserved.
+//
+
+import Foundation
+
+class FeedItem {
+    var name: String!
+    var id: Int!
+    
+    init(name: String, id: Int){
+        self.name = name
+        self.id = id
+    }
+}
---
+++ ExampleProject/ExampleProject/MockAPIService.swift
@@ -0,0 +1,59 @@
+//
+//  MockAPIService.swift
+//  ExampleProject
+//
+//  Created by Rob Seward on 8/12/15.
+//  Copyright © 2015 Electric Objects. All rights reserved.
+//
+
+import Foundation
+
+class MockAPIService {
+    static let sharedService = MockAPIService()
+    var mockFeed = [FeedItem]()
+    
+    init(){
+        _initializeMockFeed()
+    }
+    
+    func _initializeMockFeed(){
+
+        if let path = NSBundle.mainBundle().pathForResource("name_list", ofType: "txt")
+        {
+            let url = NSURL(fileURLWithPath: path)
+            do {
+                let fileContents = try NSString(contentsOfURL: url, encoding: 0)
+                let list = fileContents.componentsSeparatedByString("\n")
+                var count = 1
+                for name in list {
+                    let item = FeedItem(name: name, id: count)
+                    mockFeed.append(item)
+                    count++
+                }
+                mockFeed = mockFeed.reverse()
+            } catch let error as NSError {
+                assert(false, "There was a problem initializing the mock feed:\n\n \(error)")
+            }
+        }
+
+    }
+    
+    func fetchFeed(minID: Int, maxID: Int, count: Int, success:([FeedItem])->()) {
+        let results = mockFeed.filter({$0.id < maxID && $0.id > minID} )
+        let delayTime = Double(arc4random_uniform(100)) / 100.0
+        _delay(delayTime) { () -> () in
+            success(results)
+        }
+    }
+    
+    
+    func _delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
+    
+}
