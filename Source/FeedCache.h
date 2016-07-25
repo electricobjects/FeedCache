eb61969e906184a5eb0716ae57ba47855335f6f1
Rob Seward <rob@electricobjects.com>
2016-07-25 16:00:04 -0400

    Update comments in FeedCache.h

--- Source/FeedCache.h
+++ Source/FeedCache.h
@@ -1,6 +1,6 @@
 //
-//  FeedKit.h
-//  FeedKit
+//  FeedCache.h
+//  FeedCache
 //
 //  Created by Rob Seward on 7/16/15.
 //  Copyright (c) 2015 Rob Seward. All rights reserved.
@@ -8,12 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for FeedKit.
+//! Project version number for FeedCache.
 FOUNDATION_EXPORT double FeedCacheVersionNumber;
 
-//! Project version string for FeedKit.
+//! Project version string for FeedCache.
 FOUNDATION_EXPORT const unsigned char FeedCacheVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <FeedKit/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <FeedCache/PublicHeader.h>
 
 
